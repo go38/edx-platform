97d50a378f1a4ce92003011debd34578cc850e6f
Diana Huang <dkh@edx.org>
2015-04-29 11:01:11 -0400

    Add in the ability to get the blocks in order.

--- lms/djangoapps/instructor_task/tasks_helper.py
+++ lms/djangoapps/instructor_task/tasks_helper.py
@@ -724,12 +724,11 @@ def upload_problem_grade_report(_xmodule_instance_args, _entry_id, course_id, _t
         blocks = course_structure['blocks']
         problems = OrderedDict()
         for block in blocks:
-            if blocks[block]['type'] == 'problem' and blocks[block]['graded']:
+            if blocks[block]['type'] == 'problem' and blocks[block]['graded'] == True:
                 header_name = "{}".format(blocks[block]['display_name'])
                 problems[block] = header_name
-    # TODO: Do something more reasonable here
     except errors.CourseStructureNotAvailableError:
-        return task_progress.update_task_state(extra_meta={'step': 'Waiting for course structure'})
+        return task_progress.update_task_state(extra_meta={'step': 'Generating course structure. Please refresh and try again.'})
 
     # Just generate the static fields for now.
     rows = [list(header_row.values()) + list(problems.values())]
